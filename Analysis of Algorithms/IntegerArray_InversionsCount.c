7a76375cfd8077eb65633d3bc7fc3c1de9cc8944
NAGA GANESH KURAPATI <ganeshkurapati@gmail.com>
2016-07-27 14:38:36 -0400

    Added inversion count from Integer Array from a File

--- Analysis of Algorithms/IntegerArray_InversionsCount.c
+++ Analysis of Algorithms/IntegerArray_InversionsCount.c
@@ -1,27 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
-int mergesort(int *At,int p,int q);
-int merge(int *At,int p,int q,int r);
+long long int mergesort(int *At,int p,int q);
+long long int merge(int *At,int p,int q,int r);
 int main()
 {
-    int k ,inversions;
-    //int A[11]={143,9,7,999,37,88,222,335,66,0,0};
-    int A[9]={14,2,3,4,5,6,7,8,9};
-    for(k=0; k < (sizeof(A) / sizeof(A[0]));k++)
+    int k ;
+    long long int inversions;
+    int A[100000] = {0};
+    FILE *fptr;
+    fptr = fopen("IntegerArray.txt" , "r");
+    if (fptr == NULL)
+    {
+    printf("\n Can't open the file \n");
+    exit(1);
+    }
+    int i = 0;
+    while (!feof(fptr))
+    {
+        fscanf(fptr, "%d", &A[i++]);
+    }
+    fclose(fptr);
+    /*for(k=0; k < (sizeof(A) / sizeof(A[0]));k++)
         printf("%d,",A[k]);
-    printf("\n");
+    printf("\n");*/
     int lengthof_A = sizeof(A) / sizeof(A[0]);
     inversions = mergesort( A,0,(lengthof_A-1));
     for(k=0; k < lengthof_A ;k++)
         printf("%d,",A[k]);
-    printf(" \n %d",inversions);
+    printf(" \n %lld",inversions);
     return 0;
 }
-int merge(int *At,int p,int q,int r)
+long long int merge(int *At,int p,int q,int r)
 {
     int n1, n2, i,j,k;
-    int split_inversions = 0;
+    long long int split_inversions = 0;
     n1 = q - p +1;
     n2 = r - q ;
     int L[n1+1],R[n2+1];
@@ -48,11 +61,11 @@ int merge(int *At,int p,int q,int r)
     }
     return split_inversions;
 }
-int mergesort(int *At,int p,int r)
+long long int mergesort(int *At,int p,int r)
 {
     if(p < r){
        int q = (p + r) / 2;
-       int x,y,z;
+       long long int x,y,z;
        x = mergesort(At, p, q);
        y = mergesort(At, q + 1, r);
        z = merge(At, p, q, r);
